57441084f093702b749796dbdf8ade9101b6d8e3
xiaohai-huang <n10172912@qut.edu.au>
2019-10-12 19:13:33 +1000

    add c# file

---
+++ image_recognition_Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace image_recognition_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
